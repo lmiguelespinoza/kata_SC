4986787aa1770087b3030122f8664c8581d77a29
lmiguelespinoza <lmiguelespinoza@gmail.com>
2012-09-12 21:33:37 -0500

    Kata String Calculator

--- calcula.rb
+++ calcula.rb
@@ -1,19 +1,5 @@
 class Calcula
-	NumVal=[ ['1',1] , ['2',2] , ['3',3] ] 
 	def suma(cadena)
-		res=0
-		pos=0
-		len=cadena.length 
-		while len>0
-			dat=cadena[pos,1]
-			NumVal.each do |simbolo,valor|	
-				if  dat==simbolo
-					res+=valor 
-				end	
-			end
-			len-=1
-			pos+=1
-		end
-		return res
+		return 0
 	end	
 end
