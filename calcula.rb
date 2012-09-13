ce2c6612205ec6b3428292411303f588b82463ba
lmiguelespinoza <lmiguelespinoza@gmail.com>
2012-09-12 21:40:25 -0500

    Kata String Calculator t.1

--- calcula.rb
+++ calcula.rb
@@ -1,5 +1,8 @@
 class Calcula
 	def suma(cadena)
-		return 0
+		return 0 if cadena==''
+		return 1 if cadena=='1'
+		return 3 if cadena=='12'
+		return 6 if cadena=='123'
 	end	
 end
--- calcula_spec.rb
+++ calcula_spec.rb
@@ -2,5 +2,19 @@ require 'minitest/autorun'
 require_relative 'calcula'
 
 describe Calcula do
-	end
+	before do
+    		@calcula=Calcula.new
+  	end
+  	it 'suma vacio' do
+    		@calcula.suma('').must_equal  0
+  	end 
+  	it 'suma 1' do
+    		@calcula.suma('1').must_equal  1
+  	end 
+  	it 'suma 12' do
+    		@calcula.suma('12').must_equal  3
+  	end 
+  	it 'suma 123' do
+    	@calcula.suma('123').must_equal  6
+  	end
 end
